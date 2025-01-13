dcd5c86a8b5f24b75fdda47a187fbc086fef38bf
Maya Chan <mayachanym@gmail.com>
2025-01-13 03:25:39 +0000

    13/1: Add api search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,89 @@
-'use client';
+"use client";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
-  const [data, setData] = React.useState("Not Found");
+  const [data, setData] = useState("Not Found");
+  const [addItemState, setAddItemState] = useState(false);
+  const [productInfo, setProductInfo] = useState({});
+  const [expiryDate, setExpiryDate] = useState(new Date());
+
+  const addItem = () => {
+    setAddItemState(!addItemState);
+  };
+
+  const fetchProductDetails = (barcode) => {
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API response:", data); // Log the full API response
+        if (data.status === 1) {
+          setProductInfo(data.product);
+        } else {
+          setProductInfo({});
+          setData("Product not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching product details:", error);
+        setData("Failed to fetch product details");
+      });
+  };
+
+  const saveItem = () => {
+
+  }
+
   return (
-    <>
-     <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-    </>
+    <div className="flex p-4 w-full flex-col">
+      <div className="flex justify-end w-full">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={addItem}
+        >
+          Add items
+        </button>
+      </div>
+
+      {addItemState && (
+        <div className="flex pt-4 flex-col">
+          <BarcodeScannerComponent
+            width={500}
+            height={500}
+            onUpdate={(err, result) => {
+              if (result) {
+                setData(result.text);
+                fetchProductDetails(result.text);
+              } else {
+                setData("Not Found");
+              }
+            }}
+          />
+          <p>{data}</p>
+          {Object.keys(productInfo).length > 0 && (
+            <div>
+              <h3>Product Details:</h3>
+              <p>Name: {productInfo.product_name}</p>
+              <p>Brand: {productInfo.brands}</p>
+              <label htmlFor="expiryDate">Expiry Date: </label>
+              <DatePicker
+                selected={expiryDate} // This should be a state variable you define
+                onChange={(date) => setExpiryDate(date)} // Update the state with the new date
+                dateFormat="MMMM d, yyyy"
+                className="input"
+              />
+              <button>
+              Button
+            </button>
+            </div>
+            
+          )}
+        </div>
+      )}
+    </div>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
     //     <Image
